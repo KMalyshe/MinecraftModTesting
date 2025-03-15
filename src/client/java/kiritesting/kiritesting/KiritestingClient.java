6232f35d7b1742fbb0fd89756b5a8bb26aca7d1a
KMalyshe <kyryl.malyshev@gmail.com>
2025-03-15 11:21:09 +0100

    first commit

---
+++ src/client/java/kiritesting/kiritesting/KiritestingClient.java
@@ -0,0 +1,10 @@
+package kiritesting.kiritesting;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KiritestingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
